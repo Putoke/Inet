96df9a48e91b3032379fa319f7f63a9feeb1005f
Joakim Edlund <joakim.en.edlund@gmail.com>
2015-01-28 17:22:32 +0100

    updated new server file

--- L2/NewServer.java
+++ L2/NewServer.java
@@ -11,17 +11,21 @@ import java.util.StringTokenizer;
 
 public class NewServer{
     public  static void main(String[] args) throws java.io.IOException{
+        System.out.println("Skapar Serversocket");
         ServerSocket ss = new ServerSocket(8080);
         while(true) {
+            System.out.println("Väntar på klient...");
             Socket s = ss.accept();
+            System.out.println("Klient är ansluten");
             BufferedReader request = new BufferedReader(new InputStreamReader(s.getInputStream()));
             String str = request.readLine();
             StringTokenizer tokens = new StringTokenizer(str, "? ");
             tokens.nextToken(); // Ordet GET
             String requestedDocument = tokens.nextToken();
             while( (str = request.readLine()) != null && str.length() > 0) {
-                System.out.println();
+                System.out.println(str);
             }
+            System.out.println("Förfrågan klar.");
             s.shutdownInput();
 
             PrintStream response = new PrintStream(s.getOutputStream());
@@ -32,7 +36,7 @@ public class NewServer{
             if(requestedDocument.indexOf(".gif") != -1)
                 response.println("Content-Type: image/gif");
 
-            response.println("Set-Cookie: clientId=1; expires=Wednesday, 31-Dec-12 21:00:00 GMT");
+            response.println("Set-Cookie: clientId=1; expires=Wednesday, 31-Dec-15 21:00:00 GMT");
 
             response.println();
             File f = new File("." + requestedDocument);
