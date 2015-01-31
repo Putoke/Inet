ae0e37f4e6f1bd376b47367ad4e536f5532ac288
Joakim Edlund <joakim.en.edlund@gmail.com>
2015-01-31 16:03:03 +0100

    superhaxxor

--- L2/Server.java
+++ L2/Server.java
@@ -1,91 +1,138 @@
-import com.sun.net.httpserver.HttpExchange;
-import com.sun.net.httpserver.HttpHandler;
-import com.sun.net.httpserver.HttpServer;
-
+import java.io.*;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.OutputStream;
-import java.net.InetSocketAddress;
-import java.net.URI;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.lang.Integer;
+import java.lang.System;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashMap;
+import java.util.StringTokenizer;
 import java.util.Random;
 
 public class Server {
 
-    private HttpServer httpServer;
+    private class Tuple {
+        public int x, y;
 
-    static String[] superior = {"0usqWWfp4D0", "e-JHfXVlkik", "bX-h8CrIyyY"};
-    static int idx;
-
-    public Server () throws IOException {
-        httpServer = HttpServer.create(new InetSocketAddress(6112), 0);
-        httpServer.createContext("/", new Handler());
-        httpServer.setExecutor(null);
-        httpServer.start();
-        idx = new Random().nextInt(superior.length);
+        public Tuple(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
     }
 
 
-    static class Handler implements HttpHandler {
-        @Override
-        public void handle(HttpExchange exchange) throws IOException {
 
+    public ServerSocket ss = new ServerSocket(8080);
+    public int current_guess = -1;
+    public HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+    public HashMap<Integer, Tuple> map2 = new HashMap<Integer, Tuple>();
 
-            File currentDirFile = new File("");
-            String root = currentDirFile.getAbsolutePath();
-            if(!root.contains("src"))
-                root = root + "\\src\\";
-            URI uri = exchange.getRequestURI();
-            File file = new File(root + uri.getPath()).getCanonicalFile();
-            if (!file.getPath().startsWith(root)) {
-                // Suspected path traversal attack: reject with 403 error.
-                String response = "403 (Forbidden)\n";
-                exchange.sendResponseHeaders(403, response.length());
-                OutputStream os = exchange.getResponseBody();
-                os.write(response.getBytes());
-                os.close();
-            } else if (!file.isFile()) {
-                // Object does not exist or is not a file: reject with 404 error.
-                String response = "404 (Not Found)\n";
-                exchange.sendResponseHeaders(404, response.length());
-                OutputStream os = exchange.getResponseBody();
-                os.write(response.getBytes());
-                os.close();
-            } else {
-                // Object exists and is a file: accept with response code 200.
-                exchange.sendResponseHeaders(200, 0);
-                OutputStream os = exchange.getResponseBody();
-                FileInputStream fs = new FileInputStream(file);
-                final byte[] buffer = new byte[0x10000];
-                int count = 0;
-                while ((count = fs.read(buffer)) >= 0) {
-                    os.write(buffer,0,count);
-                }
+    public Server() throws IOException {
+        run();
+    }
 
-                os.write(("<iframe width='0' height='0' src='//www.youtube.com/embed/" + superior[idx] + "?autoplay=1&loop=1' frameborder='0' allowfullscreen></iframe>").getBytes());
-                fs.close();
-                os.close();
+    public void run() throws IOException {
+
+        System.out.println("Skapar Serversocket");
+        int id_counter = 0;
+        int current_id = -1;
+
+        while(true) {
+            System.out.println("Väntar på klient...");
+            Socket s = ss.accept();
+            current_id = -1;
+
+            System.out.println("Klient är ansluten");
+            BufferedReader request = new BufferedReader(new InputStreamReader(s.getInputStream()));
+            String str = request.readLine();
+            StringTokenizer tokens = new StringTokenizer(str, "? ");
+            tokens.nextToken(); // Ordet GET
+            String requestedDocument = tokens.nextToken();
+            String guess = tokens.nextToken();
+            if(guess.contains("numberguess=")) {
+                guess = guess.replace("numberguess=", "");
+                current_guess = Integer.parseInt(guess);
             }
-/*
+            while( (str = request.readLine()) != null && str.length() > 0) {
+                System.out.println(str);
+                if(str.contains("Cookie: clientId=")) {
+                    str = str.replace("Cookie: clientId=", "");
+                    current_id = Integer.parseInt(str);
+                    if(map.get(current_id) == null) {
+                        map.put(current_id, new Random().nextInt(100)+1);
+                        map2.put(current_id, new Tuple(-1,101));
+                    }
+                }
+            }
+            if(current_id == -1) {
+                id_counter++;
+                current_id = id_counter;
+                map.put(current_id, new Random().nextInt(100)+1);
 
-String repsonse = "testink aboo bre";
-            exchange.sendResponseHeaders(200, repsonse.length());
-            OutputStream os = exchange.getResponseBody();
-            os.write(repsonse.getBytes());
-            os.close();*/
+            }
+
+            if(current_guess == -1337) {
+                map.put(current_id, new Random().nextInt(100)+1);
+                map2.put(current_id, new Tuple(-1,101));
+                current_guess = -1;
+            }
+
+            System.out.println("Förfrågan klar.");
+            s.shutdownInput();
+
+            PrintStream response = new PrintStream(s.getOutputStream());
+            response.println("HTTP/1.1 200 OK");
+            response.println("Server : Slask 0.1 Beta");
+            if(requestedDocument.indexOf(".html") != -1)
+                response.println("COntent-Type: text/html");
+            if(requestedDocument.indexOf(".gif") != -1)
+                response.println("Content-Type: image/gif");
+
+            response.println("Set-Cookie: clientId="+current_id+"; expires=Wednesday, 31-Dec-15 21:00:00 GMT");
+
+
+            response.println();
+            File f = new File("." + requestedDocument);
+            FileInputStream infil = new FileInputStream(f);
+            byte[]b = new byte[1024];
+            while( infil.available() > 0 ){
+                response.write(b, 0, infil.read(b));
+            }
+            if(current_guess != -1) {
+                response.write("<br>".getBytes());
+                int low = map2.get(current_id).x, high = map2.get(current_id).y;
+                Tuple t = map2.get(current_id);
+                if (current_guess == map.get(current_id)) {
+                    response.write("Yay you guessed right!".getBytes());
+                }
+                if (current_guess > map.get(current_id)) {
+                    t.y = high < current_guess ? high : current_guess;
+                    if(t.x < 0 || t.y > 100)
+                        response.write("Too high".getBytes());
+                    else
+                        response.write(("Too high, guess a number between " + t.x + " and " + t.y).getBytes());
+                }
+                if (current_guess < map.get(current_id)) {
+                    t.x = low > current_guess ? low : current_guess;
+                    if (t.x < 0 || t.y > 100)
+                        response.write("Too low".getBytes());
+                    else
+                        response.write(("Too low, guess a number between " + t.x + " and " + t.y).getBytes());
+                }
+            }
+
+            s.shutdownOutput();
+            s.close();
         }
+
     }
 
-    public static void main(String[] args) {
-        try {
-            new Server();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+
+    public  static void main(String[] args) throws IOException {
+        new Server();
     }
-
-
-
-
 }
-
