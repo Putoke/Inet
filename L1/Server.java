ae7301d80e0abaa7f09f49f25bf4cf845e0dd649
Putoke <joakim.en.edlund@gmail.com>
2015-02-02 15:43:06 +0100

    lab fixes

--- L1/Server.java
+++ L1/Server.java
@@ -3,31 +3,28 @@ import java.io.*;
 import java.util.ArrayList;
 
 public class Server {
-    private static ArrayList<Socket> sockets = new ArrayList<Socket>();
+	private static ArrayList<Socket> sockets = new ArrayList<Socket>();
 
-    public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException {
         
-        if (args.length != 1) {
-            System.err.println("Usage: java EchoServer <port number>");
-            System.exit(1);
-        }
-        int portNumber = Integer.parseInt(args[0]);
-        boolean listening = true;
+		if (args.length != 1) {
+			System.err.println("Usage: java EchoServer <port number>");
+			System.exit(1);
+		}
+		int portNumber = Integer.parseInt(args[0]);
+		boolean listening = true;
 
-
-
-
-        try (ServerSocket serverSocket = new ServerSocket(portNumber)) {
-                    while (listening) {
-                        Socket s = serverSocket.accept();
-                        sockets.add(s);
-                        new ServerThread(s).start();
-                    }
-                } catch (IOException e) {
-                        System.err.println("Could not listen on port " + portNumber);
-                        System.exit(-1);
-                    }
-    }
+		try (ServerSocket serverSocket = new ServerSocket(portNumber)) {
+			while (listening) {
+				Socket s = serverSocket.accept();
+				sockets.add(s);
+				new ServerThread(s).start();
+			}
+		} catch (IOException e) {
+			System.err.println("Could not listen on port " + portNumber);
+			System.exit(-1);
+		}
+	}
 
     public static synchronized void broadcast(Socket s, String message) {
         for (Socket socket : sockets) {
