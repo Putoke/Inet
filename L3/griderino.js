b85917b6c45a2b016dc5ba3f622c1eff67e668f7
Joakim Edlund <joakim.en.edlund@gmail.com>
2015-02-04 17:51:03 +0100

    lab 3 done

--- L3/griderino.js
+++ L3/griderino.js
@@ -9,11 +9,44 @@ for (var i = 0; i < 10; i++) {
     chips[i] = new Array(10);
 }
 
-for(var i=0; i<10; i++) {
-    var x = Math.floor((Math.random() * 9) + 1);
-    var y = Math.floor((Math.random() * 9) + 1);
+function init() {
+    var x = Math.floor((Math.random() * 8));
+    var y = 0;
 
     chips[x][y] = true;
+    chips[x+1][y] = true;
+
+    x = Math.floor((Math.random() * 7));
+    y = Math.floor((Math.random() * 2) + 1);
+
+    chips[x][y] = true;
+    chips[x+1][y] = true;
+    chips[x+2][y] = true;
+
+    x = Math.floor((Math.random() * 3));
+    y = Math.floor((Math.random() * 4) + 3);
+
+    chips[x][y] = true;
+    chips[x][y+1] = true;
+    chips[x][y+2] = true;
+
+    x = Math.floor((Math.random() * 3) + 3);
+    y = Math.floor((Math.random() * 3) + 3);
+
+    chips[x][y] = true;
+    chips[x+1][y] = true;
+    chips[x+2][y] = true;
+    chips[x+3][y] = true;
+
+    x = Math.floor((Math.random() * 2) + 3);
+    y = Math.floor((Math.random() * 3) + 6);
+
+    chips[x][y] = true;
+    chips[x+1][y] = true;
+    chips[x+2][y] = true;
+    chips[x+3][y] = true;
+    chips[x+4][y] = true;
+
 }
 
 function resetDaBoard() {
@@ -22,16 +55,17 @@ function resetDaBoard() {
     }
     var buttons = document.getElementsByName("sinkedShip");
     for (var i = 0; i < buttons.length; i++) {
-        if(buttons[i].style.color == "blue" || buttons[i].style.color == "yellow") {
+        /*if(buttons[i].style.color == "blue" || buttons[i].style.color == "yellow") {
             var x = buttons[i].value.charAt(0);
             var y = buttons[i].value.charAt(2);
             chips[x][y] = true;
-        }
+        }*/
         buttons[i].style.color = "";
         buttons[i].style.background = "";
     }
     shotsFired = 0;
     hits = 0;
+    init();
     document.getElementsByName("pressed")[0].value = "";
 }
 
