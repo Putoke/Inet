9937fdadf8c3884d93965b19e4d4b9ad98f1aaa6
Fredrik Berntsson <fredrik.kona@gmail.com>
2015-02-23 14:45:23 +0100

    fisk

---
+++ L6/Server.java
@@ -0,0 +1,17 @@
+import java.security.*;
+import javax.net.ssl.*;
+import java.io.*;
+
+public class Server {
+
+
+	public static void main(String [] args) throws KeyStoreException {
+		
+		System.out.println("fisk");
+		
+		KeyStore k = KeyStore.getInstance("JKS");
+		
+	
+		}
+
+}
