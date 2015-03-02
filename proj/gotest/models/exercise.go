2c0941643e47b8e524377d6f9c1a842f36945abc
Putoke <joakim.en.edlund@gmail.com>
2015-03-02 13:51:49 +0100

    moved proj

--- proj/gotest/models/exercise.go
+++
@@ -1,7 +0,0 @@
-package models
-
-type Exercise struct {
-    Userid string `json:"userid"`
-    Name string `json:"name"`
-    Musclegroup string `json:"musclegroup"`
-}
--- proj/tomcat/bin/compile.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/csh -f
-dir=$( cd "$( dirname "$0" )" && pwd )
-tomcatpath=$dir/../lib
-webapppath=$dir/../webapps/ROOT/WEB-INF/classes
-find $webapppath -name "*.java" > sources.txt
-javac -cp $tomcatpath/servlet-api.jar @sources.txt
